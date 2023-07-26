ff59db24b8aac4fcc20d7079d3e5d67d53d70101
Tulur <evtelizhenko@gmail.com>
2023-07-26 16:54:19 +0300

    added changes to do last difficalt in level_one task

--- app/src/main/java/com/example/test_task_android/MainActivity.java
+++ app/src/main/java/com/example/test_task_android/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.test_task_android;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,10 +37,32 @@ public class MainActivity extends AppCompatActivity {
             clickCount = savedInstanceState.getInt(KEY_CLICK_COUNT, 0);
             textViewCounter.setText(String.valueOf(clickCount));
         }
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt(KEY_CLICK_COUNT, clickCount);
+    }
+    private void openFragment() {
+        MyFragment fragment = new MyFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+        fragmentTransaction.addToBackStack(null); // Add the transaction to the back stack
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Handle the back arrow click in the fragment
+        if (item.getItemId() == android.R.id.home) {
+            // Go back to the activity
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
