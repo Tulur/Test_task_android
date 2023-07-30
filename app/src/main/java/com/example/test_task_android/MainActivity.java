29c440f39782f55be25ba6759c537c18cd0d6cfe
Tulur <evtelizhenko@gmail.com>
2023-07-30 22:46:57 +0300

    Created Main Activity

---
+++ app/src/main/java/com/example/test_task_android/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.test_task_android;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new AnimalFragment())
+                    .commit();
+        }
+    }
+}
